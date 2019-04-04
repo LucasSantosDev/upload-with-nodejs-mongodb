06b287821176a23c237f6b4bc1bf6bc54acb4282
LucasSantosDev <lucas_ss.arts@live.com>
2019-04-04 15:05:38 -0300

    Layout

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('./models/db')
 
 const express = require('express')
 const path = require('path')
-const exphbs = require('express-handlebars')
+const hbs = require('express-handlebars')
 const bodyparser = require('body-parser')
 const employeeController = require('./controllers/employeeController')
 const genericRoutesController = require('./controllers/genericRoutesController')
@@ -15,17 +15,19 @@ app.use(bodyparser.json())
 
 app.set('views', path.join(__dirname, '/views/'))
 
-app.engine('ejs', exphbs({
-    extname: 'ejs',
-    defaultLayout: 'mainLayout',
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
     layoutsDir: __dirname + '/views/layouts/'
 }))
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'hbs')
 
 app.listen(3000, () => {
     console.log('Express server started at port: 3000')
 })
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', genericRoutesController)
 
